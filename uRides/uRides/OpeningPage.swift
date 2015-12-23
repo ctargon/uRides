91af60f3a941fad7fe7ec1299a29b973481b07fa
ctargon <colintargonski@Colins-Air.home>
2015-12-23 16:49:58 -0500

    NAV BAR IS GONE BITCHES
    I am proud to announce the nav bar is now eliminated from the home screen.
    
    Also added a class for the opening page.
    
    Go tigers

---
+++ uRides/uRides/OpeningPage.swift
@@ -0,0 +1,17 @@
+//
+//  OpeningPage.swift
+//  uRides
+//
+//  Created by Colin Targonski on 12/23/15.
+//  Copyright © 2015 uRides Corporation. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class OpeningPage: UIViewController {
+    
+
+    
+}
