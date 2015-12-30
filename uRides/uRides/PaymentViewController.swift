51978709be72944fd5db975119565938cebf421a
ctargon <colintargonski@Colins-Air.home>
2015-12-29 19:32:17 -0500

    Credit Card functionality!!!
    Got the payment method provided by stripe to be implemented correctly. Now need
    to clean up that interface a little bit and add our own custom UI features

--- uRides/uRides/PaymentViewController.swift
+++ uRides/uRides/PaymentViewController.swift
@@ -7,7 +7,21 @@
 import UIKit
 import Stripe
 
-class PaymentViewController: UIViewController {
+class PaymentViewController: UIViewController, STPPaymentCardTextFieldDelegate {
+    
+    @IBOutlet weak var paymentTextField: STPPaymentCardTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.paymentTextField = STPPaymentCardTextField(frame: CGRectMake(15, 15, 370, 44))
+        
+        //self.paymentTextField.delegate = self
+        
+        //self.view!.addSubview(self.paymentTextField)
+
+    }
+    
     
     
 }
---
+++ uRides/uRides/payViewer.m
@@ -0,0 +1,53 @@
+#import "ViewController.h"
+#import <Stripe/Stripe.h>
+
+@interface payViewer () <STPPaymentCardTextFieldDelegate>
+
+@property (nonatomic) STPPaymentCardTextField *paymentTextField;
+@property (nonatomic) UIButton *submitButton;
+
+@end
+
+@implementation payViewer
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.paymentTextField = [[STPPaymentCardTextField alloc]
+                             initWithFrame:CGRectMake(15, 100, CGRectGetWidth(self.view.frame) - 30, 44)];;
+    self.paymentTextField.delegate = self;
+    [self.view addSubview:self.paymentTextField];
+    
+    self.submitButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.submitButton.frame = CGRectMake(15, 150, 100, 44);
+    self.submitButton.enabled = NO;
+    [self.submitButton setTitle:@"Submit" forState:UIControlStateNormal];
+    [self.submitButton addTarget:self action:@selector(submitCard:) forControlEvents:UIControlEventTouchUpInside];
+    [self.submitButton sizeToFit];
+    [self.view addSubview:self.submitButton];
+}
+
+- (void)paymentCardTextFieldDidChange:(STPPaymentCardTextField *)textField {
+    self.submitButton.enabled = textField.isValid;
+}
+
+- (IBAction)submitCard:(id)sender {
+    // If you have your own form for getting credit card information, you can construct
+    // your own STPCardParams from number, month, year, and CVV.
+    STPCardParams* card = [self.paymentTextField card];
+    [[STPAPIClient sharedClient]
+     createTokenWithCard:card
+     completion:^(STPToken *token, NSError *error) {
+         if (token) {
+             // TODO: send the token to your server so it can create a charge
+             UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Welcome to Stripe"
+                                                                            message:[NSString stringWithFormat:@"Token created: %@", token]
+                                                                     preferredStyle:UIAlertControllerStyleAlert];
+             [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+             [self presentViewController:alert animated:YES completion:nil];
+         } else {
+             NSLog(@"Error creating token: %@", error.localizedDescription);
+         }
+     }];
+}
+@end
