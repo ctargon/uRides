8fb0088194ac85889ee61d700f811b57a074c17e
ctargon <colintargonski@Colins-Air.home>
2015-12-31 01:41:53 -0500

    got starting locations to launch first
    try to get rid of the nav bar on the map its really fucking annoying me and
    I can't get it to go away. goodnight

--- uRides/uRides/MapViewController.swift
+++ uRides/uRides/MapViewController.swift
@@ -10,11 +10,41 @@ import Foundation
 import CoreLocation
 import MapKit
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-    var locationManager: CLLocationManager!
     
+    let locationManager = CLLocationManager()
     
+    override func viewDidLoad() {
+        
+        self.locationManager.delegate = self
+        
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        self.locationManager.startUpdatingLocation()
+        
+        self.mapView.showsUserLocation = true
+    }
     
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        
+        print("Errors: " + error.localizedDescription)
+        
+    }
 }
